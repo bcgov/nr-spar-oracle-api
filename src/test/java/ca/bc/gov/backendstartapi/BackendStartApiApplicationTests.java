3522db57d06c0dac9f474864c2c9141bbe97a334
Ricardo Campos <ricardo.campos@encora.com>
2022-07-27 18:13:09 -0300

    Update with Java project instead NodeJS project
    This commit removes all the front-end and NodeJS related files.
    And also adds the Java starting project with some basic dependencies
    using Java 17, Spring WebFlux and Maven as dependency manager.

---
+++ src/test/java/ca/bc/gov/backendstartapi/BackendStartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.backendstartapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendStartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
