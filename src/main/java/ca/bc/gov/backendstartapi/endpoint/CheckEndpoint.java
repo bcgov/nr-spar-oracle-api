0fc4ff13d1a9ff57e686af1b38ee078a33b37625
Ricardo Campos <ricardo.campos@encora.com>
2022-08-25 16:07:13 -0300

    fix: release version not present at check response (#23)
    * fix: release tag not generating
    
    * feat: add release log when requesting check
    
    * fix: checkstyle import warnings
    
    * ci: change release job to merge main flow
    
    * ci: autogenerated JaCoCo coverage badge
    
    Co-authored-by: CI Bot <cibot@users.noreply.github.com>

--- src/main/java/ca/bc/gov/backendstartapi/endpoint/CheckEndpoint.java
+++ src/main/java/ca/bc/gov/backendstartapi/endpoint/CheckEndpoint.java
@@ -9,19 +9,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import ca.bc.gov.backendstartapi.response.BaseResponse;
 import ca.bc.gov.backendstartapi.vo.CheckVo;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 /**
  * This class represents a check endpoint object.
  */
+@Slf4j
 @RestController
 public class CheckEndpoint {
 
   @Value("${nrbestapi.version}")
   private String nrbestapiVersion;
 
+  /**
+   * Check if the service is up and running.
+   *
+   * @return a CheckVo object containing a message and a release version
+   */
   @GetMapping(value = "/check", produces = MediaType.APPLICATION_JSON_VALUE)
   public Mono<ResponseEntity<? extends BaseResponse>> check() {
+    log.info("nrbestapiVersion: {}", nrbestapiVersion);
     CheckVo check = CheckVo.builder().message("OK").release(nrbestapiVersion).build();
     return Mono.just(ResponseEntity.status(HttpStatus.OK).body(check));
   }
