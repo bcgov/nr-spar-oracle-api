df4ea21c41fa5380302c1bb721f3952a0384413c
Ricardo Campos <ricardo.campos@encora.com>
2022-08-25 11:23:48 -0300

    fix: discard docker entrypoint changes

--- dockerfile-entrypoint.sh
+++ dockerfile-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 DEBUG_PRINT=`$DEBUG_MODE -eq true && echo $JAVA_DEBUG_OPS || echo ""`
-echo "NRBESTAPI_VERSION: ${NRBESTAPI_VERSION}"
 
 java \
     -Djava.security.egd=file:/dev/./urandom \
