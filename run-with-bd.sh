3d00d53349161be5b0e6a87d7e9ca7628a52593c
Ricardo Campos <ricardo.campos@encora.com>
2022-11-08 13:50:22 -0300

    feat: remove run-with-bd script

--- run-with-bd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-java --source 17 InstallCert.java --quiet "${DATABASE_HOST}:${DATABASE_PORT}"
-keytool -exportcert -alias "${DATABASE_HOST}-1" -keystore jssecacerts -storepass changeit -file oracle.cer
-keytool -importcert -alias orakey -noprompt -cacerts -storepass changeit -file oracle.cer
-
-./mvnw spring-boot:run
-
