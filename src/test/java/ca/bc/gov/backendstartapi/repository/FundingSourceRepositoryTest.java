0f4b003e468459744ac2664c0bbbc2dd395ef251
Ricardo Campos <ricardo.campos@encora.com>
2023-02-14 17:47:20 -0300

    tests: add funding source repository test

---
+++ src/test/java/ca/bc/gov/backendstartapi/repository/FundingSourceRepositoryTest.java
@@ -0,0 +1,33 @@
+package ca.bc.gov.backendstartapi.repository;
+
+import ca.bc.gov.backendstartapi.entity.FundingSource;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+class FundingSourceRepositoryTest {
+
+  @Autowired
+  private FundingSourceRepository fundingSourceRepository;
+
+  @Test
+  @DisplayName("findAllTest")
+  @Sql(scripts = {"classpath:scripts/FundingSourceRepositoryTest_findAll.sql"})
+  void findAllTest() {
+    List<FundingSource> sources = fundingSourceRepository.findAll();
+
+    Assertions.assertFalse(sources.isEmpty());
+    Assertions.assertEquals(3, sources.size());
+  }
+}
