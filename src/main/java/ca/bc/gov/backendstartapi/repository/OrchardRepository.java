a50583d6a65c4df02096325a34ca3237ffb28710
Ricardo Campos <ricardo.campos@encora.com>
2023-03-03 14:18:40 -0300

    fix: add override annotation and fix sql query of orchard repository

--- src/main/java/ca/bc/gov/backendstartapi/repository/OrchardRepository.java
+++ src/main/java/ca/bc/gov/backendstartapi/repository/OrchardRepository.java
@@ -20,11 +20,13 @@ public interface OrchardRepository extends JpaRepository<Orchard, String> {
           from ORCHARD O
           join ORCHARD_LOT_TYPE_CODE T ON (
             T.ORCHARD_LOT_TYPE_CODE = O.ORCHARD_LOT_TYPE_CODE
-            AND CURRENT_DATE BETWEEN T.EFFECTIVE_DATE AND T.EXPIRY_DATE
+            AND CURRENT_DATE >= T.EFFECTIVE_DATE
+            AND CURRENT_DATE < T.EXPIRY_DATE
           )
           where O.ORCHARD_STAGE_CODE != 'RET'
             and O.ORCHARD_ID = ?1
           """,
       nativeQuery = true)
+  @Override
   Optional<Orchard> findById(String id);
 }
